c7410a8811f1a95c441b300ef8b341ba8e86e5d5
boyang.gu <boyanggu@boyanggudeMacBook-Pro.local>
2022-07-31 17:25:25 +0100

    update JSSP import issue

--- JSSP/envs/JSSP_env.py
+++ JSSP/envs/JSSP_env.py
@@ -2,6 +2,7 @@ import gym
 import numpy as np
 import plotly.figure_factory as ff
 import pandas as pd
+import random
 
 class JSSPEnv(gym.Env):
     def __init__(self, env_data):
@@ -13,14 +14,19 @@ class JSSPEnv(gym.Env):
         """
 
         # initial values for variables used for instance
-        self.jobs, self.machines = env_data[0][0], env_data[0][1]
+        self.data = env_data
+        self.jobs, self.machines = self.data[0][0], self.data[0][1]
         self.jobs_history = [[] for _ in range(self.jobs)]
-        self.machines_status = self.get_machines_status(env_data)
+        self.machines_status = self.get_machines_status()
         self.time = 0
-        self.operation_times, self.jobs_operations= self.get_operation(env_data)
+        self.operation_times, self.jobs_operations= self.get_operation()
         self.jobs_left_operations = self.jobs_operations
         self.jobs_finished_operations = [0] * self.jobs
-        self.jobs_status = [-1] * self.jobs
+        self.jobs_status = np.array([-1] * self.jobs)
+        # used for plotting
+        self.colors = [
+            tuple([random.random() for _ in range(3)]) for _ in range(self.machines)
+        ]
 
         # each row representing one job. The first entry represents whether the job is activated or not, and the second entry represents the machine it assisngs
         self.action_space = self.set_action_space(self.jobs, self.machines)
@@ -54,7 +60,6 @@ class JSSPEnv(gym.Env):
 
         return legal_actions
 
-
     def set_action_space(self, jobs, machines):
 
         lowbdd = np.zeros((jobs, 2))
@@ -65,28 +70,28 @@ class JSSPEnv(gym.Env):
 
         return action_space
 
-    def get_machines_status(self, data):
+    def get_machines_status(self):
         """
         -1 means unoccupied
         other number(eg. i) means occupied by i-th job
         """
-        m = data[0][1]
+        m = self.data[0][1]
         return np.array([-1]*m)
 
-    def get_operation(self, data):
+    def get_operation(self):
         """
         opn: operation no
         opt: operation time
         """
         n, m = self.jobs, self.machines
         opn = np.zeros(n,dtype=int)
-        maxnum = data[1][0]
+        maxnum = self.data[1][0]
 
         for i in range(n):
             now = 1
-            opn[i] = data[i+1][0]
-            if maxnum < data[i+1][0]:
-                maxnum = data[i+1][0]
+            opn[i] = self.data[i+1][0]
+            if maxnum < self.data[i+1][0]:
+                maxnum = self.data[i+1][0]
 
         opt = np.zeros((n,maxnum,m),dtype=int)
 
@@ -95,16 +100,16 @@ class JSSPEnv(gym.Env):
             now = 1
 
             for j in range(opn[i]): #j is no. of operation
-                op_number = data[i+1][now] # numbers of machine of operation
+                op_number = self.data[i+1][now] # numbers of machine of operation
                 now = now + 1
 
                 for kk in range(m):
                     opt[i][j][kk] = -1
                 for kk in range(op_number):
-                    mac_n = data[i+1][now]-1
+                    mac_n = self.data[i+1][now]-1
 
                     now = now + 1
-                    opt[i][j][mac_n] = data[i+1][now]
+                    opt[i][j][mac_n] = self.data[i+1][now]
                     now = now + 1
 
         return opt, opn
